1e3454302596751c76a9eb1a01fa7d4effbab2ce
pekkamakkonen <pek.makkonen@gmail.com>
2018-01-12 10:50:50 +0200

    Task Car Work in Progress

---
+++ object-method/TaskCar/TaskCar/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCar
+{
+    class Car
+    {
+        //Fields
+        public string Brand;
+        public int Speed;
+
+        //Contructor
+        public Car()
+        {
+            Brand = null;
+            Speed = 0;
+        }
+
+        //Methods
+        public void AskData()
+        {
+            Console.WriteLine("Anna auton merkki: ");
+            Brand = Console.ReadLine();
+            Console.WriteLine("Anna auton nopeus: ");
+            Speed = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki: {Brand}\nNopeus: {Speed}\n");
+        }
+
+        public void Accelerate()
+        {
+            double newSpeed = Speed + (Speed * 0.10);
+            Console.WriteLine($"Uusi nopeus on: { newSpeed }");
+        }
+    }
+}
---
+++ object-method/TaskCar/TaskCar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car();
+            newCar.AskData();
+            newCar.ShowCarInfo();
+            newCar.Accelerate();
+
+            Car newCar2 = new Car();
+            newCar2.AskData();
+            newCar2.ShowCarInfo();
+            newCar2.Accelerate();
+
+            Console.ReadKey();
+        }
+    }
+}
