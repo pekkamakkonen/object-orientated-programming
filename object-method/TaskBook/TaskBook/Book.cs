adbb41b12625450b37327ce079c51fd811bb1a56
pekkamakkonen <pek.makkonen@gmail.com>
2018-01-12 09:16:45 +0200

    TaskBook done

---
+++ object-method/TaskBook/TaskBook/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBook
+{
+    class Book
+    {
+        //Fields
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+        //Constuctor
+        public Book()
+        {
+            Title = null;
+            Author = null;
+            Id = 0;
+            Price = 0.00;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+
+        //Methods
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Kirjan nimi: {Title} \n Kirjailijan nimi {Author} \n ID {Id} \n Hinta {Price}");
+        }
+
+        public string CompareBook(Book book)
+        {
+            if(this.Price > book.Price)
+            {
+                return ($"{this.Title} on kalliimpi kuin {book.Title} kirja");
+            }
+            else
+            {
+                return ($"{book.Title} on kalliimpi kuin {this.Title} kirja");
+            }
+        }
+    }
+}
---
+++ object-method/TaskBook/TaskBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookMarketingAnIntroduction = new Book();
+            bookMarketingAnIntroduction.Title = "Marketing An Introduction";
+            bookMarketingAnIntroduction.Author = "Kotler Philip";
+            bookMarketingAnIntroduction.Id = 12345;
+            bookMarketingAnIntroduction.Price = 50.99;
+
+            Book bookLakiopas = new Book("Lakiopas - juridiikan perusteet", "Suojanen", 209209, 37);
+
+            Console.WriteLine(bookMarketingAnIntroduction.CompareBook(bookLakiopas));
+
+            Console.ReadKey();
+        }
+    }
+}
