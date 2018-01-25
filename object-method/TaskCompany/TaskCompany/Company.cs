0ac40863ee2723ee00d22508f3fcaba4819e328b
pekkamakkonen <pek.makkonen@gmail.com>
2018-01-25 12:43:21 +0200

    Task Company Work In Progress

---
+++ object-method/TaskCompany/TaskCompany/Company.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCompany
+{
+    class Company
+    {
+        //Fields
+        public string Title;
+        public string Address;
+        public string Phone;
+        public double Outcome;
+        public double Expense;
+
+        //Contructor
+        public Company()
+        {
+            Title = null;
+            Address = null;
+            Phone = null;
+            Outcome = 0.00;
+            Expense = 0.00;
+        }
+
+        //Methods
+        public void Profit()
+        {
+            double profit = (Outcome - Expense) / (Expense * 100);
+
+            if (profit >= 200)
+            {
+                Console.WriteLine("Firmalla menee tyydyttävästi.");
+            }
+            else if (profit >= 300)
+            {
+                Console.WriteLine("Firmalla menee hyvin.");
+            }
+            else
+            {
+                Console.WriteLine("Firmalla menee kehnosti.");
+            }
+        }
+    }
+}
---
+++ object-method/TaskCompany/TaskCompany/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TaskCompany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company companyDefault = new Company();
+            companyDefault.Title = "Firma 1";
+            companyDefault.Outcome = 200.00;
+            companyDefault.Expense = 0.00;
+            companyDefault.Profit();
+
+            Console.ReadKey();
+        }
+    }
+}
