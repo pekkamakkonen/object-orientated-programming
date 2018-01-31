74690c7399bf3b5a92e8bc87842ccdb75e03f410
pekkamakkonen <pek.makkonen@gmail.com>
2018-01-31 16:56:05 +0200

    TaskEmployee done 5/5

---
+++ object-method/TaskEmployee/TaskEmployee/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        //Fields
+        string Name;
+        int Id;
+        string Position;
+        double Salary;
+
+        //Constructor
+        public Employee()
+        {
+            Name = null;
+            Id = 0;
+            Position = null;
+            Salary = 0.00;
+        }
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+
+        //Methods
+        public string CompareSalary(Employee employee)
+        {
+            if (Salary > employee.Salary)
+            {
+                return ($"Henkilön { Name } palkka on suurempi kuin henkilön { employee.Name } palkka.");
+            }
+            else if (Salary < employee.Salary)
+            {
+                return ($"Henkilön { employee.Name } palkka on suurempi kuin henkilön { Name } palkka.");
+            }
+            else
+            {
+                return ($"Henkilön { Name } palkka on yhtäsuuri kuin henkilön { employee.Name } palkka.");
+            }
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijän nimi: {Name}\nId {Id}\nTehtävä {Position}\nPalkka {Math.Round(Salary)}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ object-method/TaskEmployee/TaskEmployee/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Luodaan tyontekijät-taulukko, jolla on kolme alkiota ja sitten sen olio-alkiot alustetaan.
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Suppo Taalasmaa", 1, "Toimitusjohtaja", 5000.00);
+            employees[1] = new Employee("Ismo Laitela", 2, "Kauppias", 2500.00);
+            employees[2] = new Employee("Kari Taalasmaa", 3, "Autonkorjaaja", 6000.00);
+
+            //Tulostetaan työntekijöiden tiedot näytölle
+
+            employees[0].PrintEmployeeInfo();
+            employees[1].PrintEmployeeInfo();
+            employees[2].PrintEmployeeInfo();
+            Console.WriteLine();
+
+            //Verrataan työntekijöiden palkkoja
+
+            Console.WriteLine(employees[0].CompareSalary(employees[1]));
+            Console.WriteLine(employees[0].CompareSalary(employees[2]));
+            Console.WriteLine(employees[1].CompareSalary(employees[2]));
+
+            Console.ReadKey();
+
+        }
+    }
+}
