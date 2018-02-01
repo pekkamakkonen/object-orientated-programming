bd905b5f69d8a820d8bd8150ac2487953ad9239f
pekkamakkonen <pek.makkonen@gmail.com>
2018-02-01 10:55:23 +0200

    Task TicketPrice WorkInProgress 4/5

---
+++ object-method/TaskTicketPrice/TaskTicketPrice/Customer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTicketPrice
+{
+    class Customer
+    {
+        //Fields
+        public string Name;
+        public int Age;
+        public bool Soldier;
+        public bool Student;
+        public bool MtkMember;
+
+        //Construction
+        public Customer()
+        {
+            Name = null;
+            Age = 0;
+            Soldier = false;
+            Student = false;
+            MtkMember = false;
+        }
+
+        public Customer(string name, int age, bool soldier, bool student, bool mtkMember)
+        {
+            Name = name;
+            Age = age;
+            Soldier = soldier;
+            Student = student;
+            MtkMember = mtkMember;
+        }
+
+        //Methods
+        public void TicketPrice()
+        {
+            double price = 16.00;
+
+            if (Age < 7)
+            {
+                price = price - (price * 1);
+            }
+            else if (Age >= 65)
+            {
+                price = price - (price * 0.5);
+            }
+            else if (Age >= 7 && Age <= 15)
+            {
+                price = price - (price * 0.5);
+            }
+            else
+            {
+                if (Soldier == true)
+                {
+                    price = price - (price * 0.5);
+                }
+                else
+                {
+                    if (Student == true)
+                    {
+                        price = price - (price * 0.45);
+                    }
+                    if (MtkMember == true)
+                    {
+                        price = price - (price * 0.15);
+                    }
+                }
+            }
+                Console.WriteLine($"Lopullinen hinta on: {price}");            
+        }
+
+        public void PrintCustomerInfo()
+        {
+            Console.WriteLine($"Asiakkaan nimi: {Name}\nÍkä: {Age}\nVarusmies: {Soldier}\nOpiskelija: {Student}\nMTK-jäsen: {MtkMember}");
+        }
+    }
+}
---
+++ object-method/TaskTicketPrice/TaskTicketPrice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskTicketPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer("Pekka", 26, true, false, true);
+            customer1.PrintCustomerInfo();
+            Console.WriteLine();
+            customer1.TicketPrice();
+            Console.ReadKey();
+        }
+    }
+}
