d43490c3f97d031b6c9ea4481ee6f580761b46a5
pekkamakkonen <pek.makkonen@gmail.com>
2018-02-02 14:20:20 +0200

    Task BookAndAuthor sector 1 done

---
+++ object-method/TaskBookAndAuthor/TaskBookAndAuthor/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBookAndAuthor
+{
+    class Book
+    {
+        //Fields
+        public string Title;
+        public string Author;
+        public string Publisher;
+        public double Price;
+        static string themeName;
+
+        //Constuctor
+        public Book()
+        {
+            Title = null;
+            Author = null;
+            Publisher = null;
+            Price = 0.00;
+        }
+
+        public Book(string title, string author, string publisher, double price)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            Price = price;
+        }
+
+        //methods
+        public void GetBook(string title)
+        {
+            if(Title == title)
+            {
+                Console.WriteLine($"Kirjan nimi: {Title}\nKirjailija: {Author}\nKustantaja: {Publisher}\nHinta: {Price}\nTeeman nimi: {themeName}");
+            }
+            else
+            {
+                Console.WriteLine("Kirjaa ei löytynyt.");
+            }
+        }
+        
+        public static void ChangeTheme(string theme)
+        {
+            themeName = theme;
+        }
+    }
+}
---
+++ object-method/TaskBookAndAuthor/TaskBookAndAuthor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskBookAndAuthor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Title = "Merkonomin matematiikka";
+            book1.Author = "Saaranen, Kolttola, Pösö";
+            book1.Publisher = "Edita Publishing Oy";
+            book1.Price = 37.94;
+
+            book1.GetBook("Merkonomin matematiikka");
+            Book.ChangeTheme("Oppikirjat");
+
+            Console.WriteLine();
+
+            book1.GetBook("Merkonomin matematiikka");
+
+            Console.ReadKey();
+        }
+    }
+}
