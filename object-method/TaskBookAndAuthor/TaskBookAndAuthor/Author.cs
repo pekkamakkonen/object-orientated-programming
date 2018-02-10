d9f88a48621bd4c9fa43578f1a61cf20ee701cce
pekkamakkonen <pek.makkonen@gmail.com>
2018-02-10 18:01:23 +0200

    Task BookAndAuthor done 4/4

---
+++ object-method/TaskBookAndAuthor/TaskBookAndAuthor/Author.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Literature.TaskBookAndAuthor;
+
+namespace TaskBookAndAuthor
+{
+    class Author
+    {
+        //fields
+        public string Name;
+        public string DateOfBirth;
+        Book Book;
+
+        //constructor
+        public Author()
+        {
+            Name = null;
+            DateOfBirth = null;
+            Book = null;
+        }
+
+        public Author(string name, string dateOfBirth, Book book)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Book = book;
+        }
+
+        //methods
+        public void PrintAuthorInfo()
+        {
+            Console.WriteLine($"Kirjailijan nimi: {Name}\nSyntymäaika: {DateOfBirth}\n\nKirjan tiedot:\n");
+
+            if (Name == Book.Author)
+            {
+                Book.PrintBookInfo();
+            }
+            else
+            {
+                Console.WriteLine("Kirjailija ei ole kirjoittanut hakuehdoissa määritettyä kirjaa.");
+            }
+        }
+    }
+}
+
