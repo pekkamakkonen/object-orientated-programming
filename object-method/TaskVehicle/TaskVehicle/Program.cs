212f2e4685b10c1a1afbfaebfa071578ada48718
pekkamakkonen <pek.makkonen@gmail.com>
2018-02-11 11:53:13 +0200

    Task Vehicle part 1/4 done

---
+++ object-method/TaskVehicle/TaskVehicle/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TaskVehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle1 = new Vehicle("Auto", "Ferrari", 2018, 5000000.00);
+            vehicle1.PrintInfo();
+
+            Console.WriteLine();
+
+            Car car1 = new Car("V10", "5-sarja E60", 4);
+            car1.PrintInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ object-method/TaskVehicle/TaskVehicle/Vehicle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskVehicle
+{
+    class Vehicle
+    {
+        //fieds
+        protected string Type;
+        protected string Brand;
+        protected int YearModel;
+        protected double Price;
+
+        //constructor
+        public Vehicle()
+        {
+            Type = null;
+            Brand = null;
+            YearModel = 0;
+            Price = 0.00;
+        }
+
+        public Vehicle(string type, string brand, int yearModel, double price)
+        {
+            Type = type;
+            Brand = brand;
+            YearModel = yearModel;
+            Price = price;
+        }
+
+        //methods
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Ajoneuvon tyyppi: {Type}\nMerkki: {Brand}\nVuosimalli: {YearModel}\nHinta: {Price}");
+        }
+        
+    }
+    //inheritances class Car from class Vehicle
+
+    class Car : Vehicle
+    {
+        //fields
+        protected string EngineSize;
+        protected string Model;
+        protected int Doors;
+
+        //constructor
+        public Car()
+        {
+            EngineSize = null;
+            Model = null;
+            Doors = 0;
+        }
+
+        public Car(string engineSize, string model, int doors)
+        {
+            EngineSize = engineSize;
+            Model = model;
+            Doors = doors;
+        }
+
+        //overrides PrintInfo
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Moottorin koko: {EngineSize}\nMalli: {Model}\nOvien lukumäärä: {Doors}");
+        }
+    }
+}
