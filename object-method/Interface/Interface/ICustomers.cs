22a7182b7e2d2f16fa3abd5d8c1e21b2391cbd62
pekkamakkonen <pek.makkonen@gmail.com>
2018-04-14 12:27:08 +0300

    Task Interface parts 3/4 done. ICustomers and IProducts interfaces added for this version.

---
+++ object-method/Interface/Interface/ICustomers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    interface ICustomers
+    {
+        void AddCustomer(Customer customer);
+        void PrintCustomers();
+    }
+}
---
+++ object-method/Interface/Interface/IProducts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    interface IProducts
+    {
+        void AddProduct(Product product);
+        void PrintProducts();
+    }
+}
