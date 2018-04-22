d9fdda1d8fdd914647750694ac7c11337dee7c81
pekkamakkonen <pek.makkonen@gmail.com>
2018-04-22 18:15:20 +0300

    Class Truck added and inheritanced from Car-class. Task Vehicle done 4/4

--- object-method/TaskVehicle/TaskVehicle/Program.cs
+++ object-method/TaskVehicle/TaskVehicle/Program.cs
@@ -8,6 +8,9 @@ namespace TaskVehicle
         {
             Vehicle vehicle1 = new Car("Auto", "Ferrari", 2018, 5000000.00, "V10", "5-sarja E60", 4);
             vehicle1.PrintInfo();
+            Console.WriteLine();
+            Vehicle vehicle2 = new Truck("Kuorma-auto", "Iveco EuroCargo", 2004, 13500.00, "5.9 l", "", 2, 11990.00, 20);
+            vehicle2.PrintInfo();
 
             Console.WriteLine();
             Console.ReadKey();
---
+++ object-method/TaskVehicle/TaskVehicle/Truck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskVehicle
+{
+    class Truck : Car
+    {
+        //fields
+        protected double WeightOfCargo;
+        protected double ConsumptionPerKg;
+
+        //contructor
+        public Truck(string type, string brand, int yearModel, double price, string engineSize, string model, int doors, double weightOfCargo, double consumptionPerKg) : base(type, brand, yearModel, price, engineSize, model, doors)
+        {
+            WeightOfCargo = weightOfCargo;
+            ConsumptionPerKg = consumptionPerKg;
+        }
+
+        //methods
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Kuorman paino: {WeightOfCargo}\nKulutus per kg: {ConsumptionPerKg}");
+        }
+
+        protected double CalculateConsumption()
+        {
+            double consumption = WeightOfCargo / ConsumptionPerKg;
+            return consumption;
+        }
+
+
+    }
+}
